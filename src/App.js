37bad7dcfad7245039311033177b44e9a8927d7e
TS22082 <ts22082@gmail.com>
2018-09-05 16:01:39 -0700

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import firebase from "./firebase.js";
 
+const db = firebase.database();
+const auth = firebase.auth();
+
+db.ref("CON").on("value", snapshot => {
+  const val = snapshot.val();
+  console.log(val);
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      AuthStatus: false
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Login />
-        <Home />
+        {/* <Login />
+        <Home /> */}
+
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Login" component={Login} />
+          </div>
+        </Router>
       </div>
     );
   }
