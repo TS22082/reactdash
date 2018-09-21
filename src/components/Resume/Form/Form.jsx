c9b496b4167783a890aec0b5164274d1a0dda021
TS22082 <ts22082@gmail.com>
2018-09-21 13:56:47 -0700

    input from form in resume is now saved to state on change

--- src/components/Resume/Form/Form.jsx
+++ src/components/Resume/Form/Form.jsx
@@ -3,6 +3,21 @@ import "./Form.css";
 
 class Form extends Component {
   state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = { contact: "", name: "", message: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div className="contactContainer">
@@ -12,21 +27,24 @@ class Form extends Component {
             name="contact"
             placeholder="Your contact info . . ."
             className="contactInput"
+            onChange={this.handleChange}
           />
           <input
             type="text"
             name="name"
             placeholder="Your name. . ."
             className="nameInput"
+            onChange={this.handleChange}
           />
         </form>
         <textarea
-          name="messageText"
-          id="messageText"
-          className="messageText"
+          name="message"
+          id="message"
+          className="messageInput"
           placeholder="message text. . ."
           cols="30"
           rows="10"
+          onChange={this.handleChange}
         />
         <button className="btnSubmit">Submit</button>
       </div>
